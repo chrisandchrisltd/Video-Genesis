1f8d7c74bd4af3bece7596d06190f64b816bc9d7
chrisandchrisltd <ukas2000ng@gmail.com>
2026-04-14 00:36:42 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,40 @@
-import { Switch, Route, Router as WouterRouter } from "wouter"; import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; import { Toaster } from "./components/ui/toaster"; import { TooltipProvider } from "./components/ui/tooltip"; import NotFound from "./pages/not-found"; import Home from "./pages/home"; import VideoDetail from "./pages/video-detail"; import Success from "./pages/success"; import Upgrade from "./pages/upgrade";
+import { Switch, Route, Router as WouterRouter } from "wouter";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { Toaster } from "./components/ui/toaster";
+import { TooltipProvider } from "./components/ui/tooltip";
+
+import NotFound from "./pages/not-found";
+import Home from "./pages/home";
+import VideoDetail from "./pages/video-detail";
+import Success from "./pages/success";
+import Upgrade from "./pages/upgrade";
+
 const queryClient = new QueryClient();
-function Router() { return (        ); }
-function App() { return (   <WouterRouter base={import.meta.env.BASE_URL.replace(//$/, "")}>      ); }
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/video/:id" component={VideoDetail} />
+      <Route path="/success" component={Success} />
+      <Route path="/upgrade" component={Upgrade} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
+          <Router />
+        </WouterRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
 export default App;
